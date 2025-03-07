aeca470231b33f127e2eb638580afd92aed1df11
Luistorrespuebla <torrespueblal@gmail.com>
2025-03-07 10:50:46 -0600

    primera version

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '../dist/public/css/main.css';
+
+import './interfaz.js'
+
+//Swal.fire("SweetAlert2 is working!");
+
+let player1= new Game();
